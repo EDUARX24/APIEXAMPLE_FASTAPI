a4915177ad652fbf9f282571a9bb07bcf5b398c1
Eduar Garcia <eduarfrancis11@gmail.com>
2025-01-08 22:37:39 -0600

    IMP - Requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter
 #importar route
 from routes.movie import routerMovie
 from routes.users import login_users
+import uvicorn
+import os
 
 #Intsantiate the FastAPI class
 app = FastAPI(
@@ -21,3 +23,6 @@ app.include_router(login_users)
 def read_root():
     return HTMLResponse(content="<h1>FastAPI Tutorial</h1><p>Primeros pasos con FastAPI</p>")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
